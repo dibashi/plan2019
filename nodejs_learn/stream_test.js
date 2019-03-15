e75bea62eb6e69ede2ddb198d66c82286c14c8ba
dibashi <dibashi1989@163.com>
2019-03-15 11:25:11 +0800

    Update stream_test.js

--- nodejs_learn/stream_test.js
+++ nodejs_learn/stream_test.js
@@ -1,23 +1,25 @@
 var fs =require('fs');
 
 
+//等价于 fs.createReadStream(src).pipe(fs.createWriteStream(dst));
 function copy(src,dst) {
-    var rs = fs.createReadStream(src);
-    var ws = fs.createWriteStream(dst);
+    // var rs = fs.createReadStream(src);
+    // var ws = fs.createWriteStream(dst);
 
-    rs.on('data',function(chunk) {
-        if(ws.write(chunk) === false){
-            rs.pause();
-        }
-    });
+    // rs.on('data',function(chunk) {
+    //     if(ws.write(chunk) === false){
+    //         rs.pause();
+    //     }
+    // });
 
-    rs.on('end',function() {
-        ws.end();
-    });
+    // rs.on('end',function() {
+    //     ws.end();
+    // });
 
-    ws.on('drain',function() {
-        rs.resume();
-    })
+    // ws.on('drain',function() {
+    //     rs.resume();
+    // })
+    fs.createReadStream(src).pipe(fs.createWriteStream(dst));
 }
 
 function main(argv) {
